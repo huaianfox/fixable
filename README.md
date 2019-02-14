f2d946d988a229bb79daf8f52882ce640cb5f99d
huaianfox <zqingl@163.com>
2019-02-14 13:05:37 +0800

    version1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# fixable
+Build fixable scale page on mobile platform
+## Usage
+
+默认设计稿750px,通过计算屏幕宽window.screen.width和设计稿的比例动态修改vieport的缩放达到页面的等比缩放，不需要rem，默认字号推荐设置24px以上。
+
+#### Import
+
+```html
+<script src="/fixable.min.js"></script>
+or
+<meta name="viewport" view-width='640'>
+<script src="/fixable.min.js"></script>
+```
+
---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ @Name：fixable v1.0.0 固定尺寸比例伸缩页面
+ @Author：huaianfox
+ @email: zqingl@163.com
+ @License：MIT
+ */
+
+(function fixable(window, document) {
+  var rescale = 0,
+    metaStr = '',
+    meta = Array.prototype.slice.call(document.querySelectorAll('meta[name="viewport"]'), -1)[0] || function () {
+      var meta = document.createElement('meta');
+      meta.name = 'viewport';
+      document.querySelector('head').appendChild(meta);
+      return meta;
+    }(),
+    setScale = function () {
+      var scale = window.screen.width / width;
+      if (rescale === scale) {
+        return;
+      }
+      rescale = scale;
+      meta.content = metaStr.replace(/\{s\}/g, scale);
+    };
+  width = parseInt(meta.getAttribute("view-width")) || 750;
+  metaStr = 'width=' + width + ',initial-scale={s},minimum-scale={s},maximum-scale={s},user-scalable=no',
+    throttle = function (func, wait) {
+      var timeout = null;
+      return function () {
+        var content = this, args = arguments;
+        if (!timeout) {
+          timeout = setTimeout(function () {
+            timeout = null;
+            func.apply(content, args);
+          }, wait)
+        }
+      }
+    };
+
+  setScale();
+
+  // reset scale on page resize, throttle
+  window.addEventListener('resize', throttle(setScale, 100), false);
+
+  // page from cache or back
+  window.addEventListener('pageshow', function (e) {
+    if (e.persisted || window.performance &&
+      window.performance.navigation.type == 2) {
+      setScale()
+    }
+  }, false);
+
+})(window, document);
